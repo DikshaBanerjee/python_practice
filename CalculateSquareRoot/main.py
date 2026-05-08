247da5a48a70100f4f906908b8c157a3d2dfd302
Diksha Banerjee <dikshabanerjee4@gmail.com>
2026-05-08 11:28:11 +0530

    Calculation of Square root of a number

---
+++ CalculateSquareRoot/main.py
@@ -0,0 +1,10 @@
+import math
+
+num = int(input("Enter a number: "))
+sr = num ** 0.5
+print("Square root of the given number:", sr)
+
+# using math module
+num1 = int(input("Enter a number here: "))
+sr1 = math.sqrt(num1)
+print("Square root of given number is:", sr1)
