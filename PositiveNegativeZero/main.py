1d23504e1211004989477660d3d1184d02bf9af9
Diksha Banerjee <dikshabanerjee4@gmail.com>
2026-05-16 20:31:02 +0530

    Number is Positive, Negative or Zero

---
+++ PositiveNegativeZero/main.py
@@ -0,0 +1,8 @@
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("Your number is a Positive number.")
+elif num < 0:
+    print("Your number is a Negative number.")
+else:
+    print("Your number is Zero.")    
