a10e683ad5bb5ea4cdc8b1181a43aeff0641ee4c
Diksha Banerjee <dikshabanerjee4@gmail.com>
2026-05-17 17:30:10 +0530

    Prime number or not

---
+++ PrimeNumber/main.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number here: "));
+
+if num == 1:
+    print("Not a prime number.");
+elif num > 1:
+    for i in range(2,num):
+        if num % i == 0:
+           print("Not a prime number.");
+           break;
+    else:
+        print("A prime number.");
+
