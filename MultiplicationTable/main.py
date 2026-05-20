5c6b2a297c260e2a34adfd45eef8c86522d1146b
Diksha Banerjee <dikshabanerjee4@gmail.com>
2026-05-20 17:32:59 +0530

    Table using for and while loop

---
+++ MultiplicationTable/main.py
@@ -0,0 +1,16 @@
+num = int(input("Enter the number here: "));
+
+for i in range(1,11):
+    table = num * i
+    print(num,"X",i,"=",table);
+
+
+# using while loop-----
+
+num2 = int(input("Enter the number here: "));
+i = 1;
+
+while i <= 10:
+    print(num,"X",i,"=",num*i)
+    i = i+1;
+
