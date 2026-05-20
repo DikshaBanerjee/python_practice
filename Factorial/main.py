9f52dc23be2a91d7412d7c597b2d1c0a559854fa
Diksha Banerjee <dikshabanerjee4@gmail.com>
2026-05-20 17:08:35 +0530

    Using recursion for factorial

--- Factorial/main.py
+++ Factorial/main.py
@@ -9,3 +9,17 @@ else:
     for i in range(1, num+1):
         fact = fact * i;
 print("Factorial of the number is", fact);
+
+
+# using recursion-----
+
+def factorial(a):
+    if a == 0:
+        return 1
+    else: 
+        return (a)*factorial(a-1);
+    
+num2 = int(input("Enter the number here: "));  
+result = factorial(num2);
+print("Factorial of the number is", result);  
+
